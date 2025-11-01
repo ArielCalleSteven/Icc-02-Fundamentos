dd3d5683dca04f0dd4a893504360afab74116fbc
diegotorres006 <jtorresm14@est.ups.edu.ec>
2025-11-01 11:13:11 -0500

    Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './features/homePage/homePage';
+import { Perfil } from './features/PerfilPage/Perfil';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'perfil',
+    component: Perfil
+  }
+];
---
+++ src/app/features/PerfilPage/Perfil.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-perfil',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './Perfil.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Perfil {
+  // señales (signals)
+  name = signal<string>('Juan');
+  lastName = signal<string>('Pérez');
+  age = signal<number>(30);
+
+  // métodos solicitados
+  getFullName(): string {
+    return `${this.name()} ${this.lastName()} con edad ${this.age()} años`;
+  }
+
+  changeData(): void {
+    this.name.set('Ana');
+    this.lastName.set('Gonzales');
+    this.age.set(25);
+  }
+
+  changeAge(): void {
+    this.age.set(18);
+  }
+
+  resetData(): void {
+    this.name.set('Juan');
+    this.lastName.set('Pérez');
+    this.age.set(30);
+  }
+}
